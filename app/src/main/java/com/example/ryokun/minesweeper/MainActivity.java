facd248cd3a4552fc7c768d5ea948cb4ec40e80e
kioke takaaki <taka.vao.321@gmail.com>
2016-06-05 17:00:03 +0900

    viewてすと

--- app/src/main/java/com/example/ryokun/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/ryokun/minesweeper/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.ryokun.minesweeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View testv = findViewById(R.id.testview);
+        testv.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Toast.makeText(getApplicationContext(), "clicked!", Toast.LENGTH_SHORT).show();
+        }});
     }
 }
