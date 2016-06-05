bf79cfdc5c303d0c47636d5cc40bf313870c3430
kioke takaaki <taka.vao.321@gmail.com>
2016-06-05 17:36:54 +0900

    レイアウトの追加に成功

--- app/src/main/java/com/example/ryokun/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/ryokun/minesweeper/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.ryokun.minesweeper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridLayout board = new GridLayout(this);
+        board.setColumnCount(5);
+        board.setRowCount(5);
+        for(int i=0; i<25; i++){
+            ImageView view = new ImageView(this);
+            view.setImageResource(R.drawable.abc_ic_star_black_36dp);
+            board.addView(view);
+        }
+
+        RelativeLayout p = (RelativeLayout)findViewById(R.id.parent);
+        p.addView(board);
 
         View testv = findViewById(R.id.testview);
         testv.setOnClickListener(new View.OnClickListener(){
